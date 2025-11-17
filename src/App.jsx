ab7a63b922d0c5fa4ee0e39fa121ca20ef9d5e73
ngoctan392k4 <ngoctan300904@gmail.com>
2025-11-18 01:11:48 +0700

    Update boundary

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,52 @@
-import { useState } from 'react'
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomeAdmin from './components/admin/Home/Home';
-import HomeInstructor from './components/instructor/Home/Home';
-import HomeStudent from './components/student/Home/Home';
+import HomeAdmin from "./components/admin/Home/Home";
+import HomeInstructor from "./components/instructor/Home/Home";
+import HomeStudent from "./components/student/Home/Home";
+import HomeLogin from "./components/auth/HomeLogin";
+import EnsureLoggedToRoutes from "./components/auth/EnsureLoggedToRoutes";
+import RoleBasedAuthorization from "./components/auth/RoleBasedAuthorization";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      {/* <HomeAdmin></HomeAdmin> */}
-      {/* <HomeStudent></HomeStudent> */}
-      {/* <HomeInstructor></HomeInstructor> */}
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<HomeLogin />} />
+      <Route
+        path="/homepageAdmin"
+        element={
+          <EnsureLoggedToRoutes>
+            <RoleBasedAuthorization allowRole={"admin"}>
+              <HomeAdmin />
+            </RoleBasedAuthorization>
+          </EnsureLoggedToRoutes>
+        }
+      />
+
+      <Route
+        path="/homepageStudent"
+        element={
+          <EnsureLoggedToRoutes>
+            <RoleBasedAuthorization allowRole={"student"}>
+              <HomeStudent />
+            </RoleBasedAuthorization>
+          </EnsureLoggedToRoutes>
+        }
+      />
+
+      <Route
+        path="/homepageInstructor"
+        element={
+          <EnsureLoggedToRoutes>
+            <RoleBasedAuthorization allowRole={"instructor"}>
+              <HomeInstructor />
+            </RoleBasedAuthorization>
+          </EnsureLoggedToRoutes>
+        }
+      />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
