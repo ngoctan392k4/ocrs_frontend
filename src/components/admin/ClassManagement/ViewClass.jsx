49d415387776c3bb0a3ff3a286f4684a4e2ea01e
Thi <mthi12347@gmail.com>
2025-11-20 14:26:07 +0700

    US27_ViewClass d and i

--- src/components/admin/ClassManagement/ViewClass.jsx
+++ src/components/admin/ClassManagement/ViewClass.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
 import Menu from "../../menu/Menu";
 import menu_admin from "../../../assets/dataMenu/MenuAdminData";
-import "../../../styles/ViewClass.css";
+import { useNavigate } from "react-router-dom";
+import "../../../styles/admin/ClassManagement/ViewClass.css";
 
 export default function ViewClass() {
+  const navigate = useNavigate();
+
   const [searched, setSearched] = useState("");
   const [selectedClasses, setSelectedClasses] = useState([]);
   const [classes, setClasses] = useState([]);
@@ -27,9 +30,13 @@ export default function ViewClass() {
     fetchClasses();
   }, []);
 
-  const searchClass = classes.filter((cls) =>
-    cls.clsid?.toLowerCase().includes(searched.toLowerCase())
-  );
+  const searchClass = classes.filter((cls) => {
+    const keyword = searched.toLowerCase();
+    return (
+      cls.clsid?.toLowerCase().includes(keyword) ||
+      cls.classname?.toLowerCase().includes(keyword)
+    );
+  });
 
   const toggleClass = (id) => {
     setSelectedClasses((otherClasses) =>
@@ -37,6 +44,47 @@ export default function ViewClass() {
         ? otherClasses.filter((clsid) => clsid !== id)
         : [...otherClasses, id]
     );
+  };
+
+  const [showDialog, setShowDialog] = useState(false);
+  const [deleteClassId, setDeleteClassId] = useState(null);
+
+  // const handleDeleteClick = () => {
+  //   setShowDialog(true);
+  // };
+
+  const handleDeleteClick = (id) => {
+    setDeleteClassId(id);
+    setShowDialog(true);
+  };
+
+  // const handleConfirm = () => {
+  //   console.log("DELETE!");
+  //   setShowDialog(false);
+  // };
+
+  const handleConfirmDelete = async () => {
+    if (!deleteClassId) return;
+
+    try {
+      await fetch(
+        `http://localhost:3001/api/admin/ClassManagement/${deleteClassId}`,
+        {
+          method: "DELETE",
+        }
+      );
+
+      await fetchClasses(); // cập nhật lại UI
+      setShowDialog(false);
+      setDeleteClassId(null);
+    } catch (err) {
+      console.error("Delete failed:", err);
+    }
+  };
+
+  const handleCancel = () => {
+    setShowDialog(false);
+    setDeleteClassId(null);
   };
 
   return (
@@ -64,36 +112,60 @@ export default function ViewClass() {
               <div className="class-header">
                 <div className="class-name">{cls.classname}</div>
 
-                <button className="delete-btn class-delete-btn">x</button>
+                <button
+                  className="delete-btn class-delete-btn"
+                  onClick={() => handleDeleteClick(cls.clsid)}
+                >
+                  x
+                </button>
               </div>
+
               {selectedClasses.includes(cls.clsid) && (
                 <div className="class-detail">
                   <div className="detail-row">
                     <span className="class-info-label">Class ID: </span>
                     <span className="class-info-text">{cls.clsid}</span>
                   </div>
+
                   <div className="detail-row">
-                    <span className="class-info-label">Class Name: </span>
-                    <span className="class-info-text">{cls.classname}</span>
+                    <span className="class-info-label">Class Code: </span>
+                    <span className="class-info-text">{cls.classcode}</span>
                   </div>
+
                   {/* <div className="detail-row">
+                    <span className="class-infor-label">Available Course ID: </span>
+                    <span className="class-infor-text">{cls.availableCourseID}</span>
+                    </div> */}
+
+                  <div className="detail-row">
                     <span className="class-info-label">
                       Available Course ID:{" "}
                     </span>
                     <span className="class-info-text">
-                      {cls.availableCourseID}
+                      {cls.availablecourseid}
                     </span>
-                  </div> */}
-                  {/* <div className="detail-row">
+                  </div>
+
+                  <div className="detail-row">
+                    <span className="class-info-label">Class Name: </span>
+                    <span className="class-info-text">{cls.classname}</span>
+                  </div>
+
+                  <div className="detail-row">
                     <span className="class-info-label">Instructor: </span>
-                    <span className="class-info-text">
-                      {cls.instructorID}
-                    </span>
-                  </div> */}
+                    <span className="class-info-text">{cls.instructorid}</span>
+                  </div>
+
+                  <div className="detail-row">
+                    <span className="class-info-label">Semester: </span>
+                    <span className="class-info-text">{cls.semid}</span>
+                  </div>
+
                   <div className="detail-row">
                     <span className="class-info-label">Schedule: </span>
                     <span className="class-info-text">{cls.schedule}</span>
                   </div>
+
                   <div className="detail-row">
                     <span className="class-info-label">
                       Class Location: <br />
@@ -102,6 +174,7 @@ export default function ViewClass() {
                       {cls.classlocation || "null"}
                     </span>
                   </div>
+
                   <div className="detail-row">
                     <span className="class-info-label">Capacity: </span>
                     <span className="class-info-text">{cls.capacity}</span>
@@ -115,8 +188,33 @@ export default function ViewClass() {
             </div>
           ))}
         </div>
+        {showDialog && (
+          <div className="dialog-backdrop">
+            <div className="dialog-box">
+              <div className="dialog-message">
+                Delete Class {deleteClassId}?
+              </div>
 
-        <button className="add-class-btn">+</button>
+              <div className="dialog-actions">
+                <button className="dialog-btn no" onClick={handleCancel}>
+                  No
+                </button>
+                <button
+                  className="dialog-btn yes"
+                  onClick={handleConfirmDelete}
+                >
+                  Yes
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+        <button
+          className="add-class-btn"
+          onClick={() => navigate("/classManagement/addClass")}
+        >
+          +
+        </button>
       </div>
     </div>
   );
